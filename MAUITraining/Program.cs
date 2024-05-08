f9b3508b38d0438ffa7dade33f3f1af69281e96d
abdulra <abdulra@ABDULRA-IN-LE01>
2024-05-08 11:09:33 +0530

    Add project files.

---
+++ MAUITraining/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MAUITraining;
+
+User u1 = new();
+
+try
+{
+    Console.Write("Enter user Id : ");
+    u1.Id = Console.ReadLine();
+    Console.Write("Enter user Name : ");
+    u1.Name = Console.ReadLine();
+    Console.Write("Enter user Ponenumber : ");
+    u1.Phonenumber = Console.ReadLine();
+}
+catch(ArgumentException e)
+{
+    Console.WriteLine(e.Message);   
+}
---
+++ MAUITraining/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUITraining
+{
+    internal class User
+    {
+        private string id;
+        private string name;
+        private string phonenumber;
+
+        public string Id { 
+            get => id;
+            set {
+                id = value;
+            }
+
+            }
+        public string Name { 
+            get => name; 
+            set {
+                name = value;
+                if (name == string.Empty){
+                    throw new ArgumentException("Name cannot be Empty");
+                }
+            } }
+        public string Phonenumber
+        {
+            get => phonenumber; 
+            set
+            {
+                name = value;
+                if (name == string.Empty)
+                {
+                    throw new ArgumentException("Phonenumber cannot be Empty");
+                }
+            }
+        }
+
+        public User() { }
+
+        public User(string id,string name, string phonenumber )
+        {
+            Id = id;
+            Name = name;
+            Phonenumber = phonenumber;
+        }
+
+        
+    }
+}
